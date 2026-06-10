0e70f493651a41ddfed05039be8bad3e4e44258f
SebastTr <164154274+SebastTr@users.noreply.github.com>
2026-06-10 22:42:52 +0200

    Implementiere Agentenfunktionalität zur Jobsuche mit Langchain

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_tavily import TavilySearch
 import os
 
 load_dotenv()
 
 
 def main():
-    print("Hello from langchain-course!")
+    llm = ChatOpenAI(model="gpt-5")
+    tools = [TavilySearch()]
+    agent = create_agent(model=llm, tools=tools)
+    result = agent.invoke(
+        {"messages": [HumanMessage(content="Search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details!")]}
+    )
+    print(result)
 
 
 if __name__ == "__main__":
